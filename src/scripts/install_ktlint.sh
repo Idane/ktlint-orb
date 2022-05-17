ad96186dec1319bd068bb70daf367ad07cc9a9a8
Idan Elhlawani <idan@elhalwani.com>
2022-05-17 21:53:57 +0300

    fix: shellcheck corrections

--- src/scripts/install_ktlint.sh
+++ src/scripts/install_ktlint.sh
@@ -4,12 +4,12 @@ else
     if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 fi
 
-if which ktlint > /dev/null; then
+if command -v ktlint >/dev/null 2>&1; then
     echo "ktlint is already installed"
     exit 0
 fi
 
-cd /tmp
+cd /tmp || { echo "Failed to switch to /tmp directory"; exit 1; }
 
 # Download ktlint
 curl -sSLO https://github.com/pinterest/ktlint/releases/download/0.45.2/ktlint
